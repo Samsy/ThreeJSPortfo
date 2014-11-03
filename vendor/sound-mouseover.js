6ec22195b49abc7ec6db50eb10e24ae005069aae
samuel <samuel@grouek.com>
2014-11-03 13:04:55 +0100

    protfo

---
+++ vendor/sound-mouseover.js
@@ -0,0 +1,21 @@
+<!--
+
+function playclip() {
+if (navigator.appName == "Microsoft Internet Explorer") {
+if (document.all)
+ {
+  document.all.sound.src = "click.wav";
+ }
+}
+
+else {
+{
+var audio = document.getElementsByTagName("audio")[0];
+audio.play();
+}
+}
+}
+
+//-->
+
+
